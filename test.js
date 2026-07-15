b193e4fc08c2d9f95bef38c2ce6a8842388a38e1
Harsh Pathak <harphy45@gmail.com>
2026-07-15 22:41:50 +0530

    resume form appreance done

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 import getData from "./scrap.js";
 import parseJobPage from "./parser.js";
+
+
 chrome.action.onClicked.addListener(async (tab) => { 
+    const resume = await chrome.storage.local.get(["resume"]);
+    if(!resume.resume) {
+        console.log("resume not found");
+        chrome.tabs.create({ url: "input.html" });
+        return;
+    }
+    const resumeData = resume.resume.data;
     const data = await getData(tab.id);
-    
     const parsedData = parseJobPage(data.toString());
     if(parsedData) {
         console.log(parsedData)
